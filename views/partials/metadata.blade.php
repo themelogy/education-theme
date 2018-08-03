db302380a875f1cea06caf4ab075bfadad7006e1
sagsoz06 <designer@visualturk.net>
2018-08-03 16:21:36 +0300

    Updating metada

--- views/partials/metadata.blade.php
+++ views/partials/metadata.blade.php
@@ -17,10 +17,4 @@
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
 
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PHW8Q32');</script>
-<!-- End Google Tag Manager -->
+{!! setting('core::google-tag-manager') !!}
