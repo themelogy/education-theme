906f527ea09e13931879d6577399bd26c47f1d34
sagsoz06 <designer@visualturk.net>
2020-01-08 03:05:54 +0300

    Added template without Header and Footer

---
+++ views/layouts/custom.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ LaravelLocalization::getCurrentLocale() }}">
+<head>
+    @include('partials.metadata')
+</head>
+
+<body id="top" class="has-header-search">
+
+@yield('content')
+
+@include('partials.scripts')
+</body>
+
+</html>
